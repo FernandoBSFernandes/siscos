fc538d79bdc72d65f47f1fbd118ef035a93d1484
Fernando Barros <fernandes.nando@outlook.com>
2017-11-17 07:26:57 -0200

    Continuação da tela de atendimento.

--- src/main/java/br/com/softwareservice/controle/AtendimentoBean.java
+++ src/main/java/br/com/softwareservice/controle/AtendimentoBean.java
@@ -6,11 +6,15 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import org.primefaces.component.inputtext.InputText;
 
 import br.com.softwareservice.dao.DAOFacade;
 import br.com.softwareservice.dao.IDAO;
 import br.com.softwareservice.entidades.Atendimento;
 import br.com.softwareservice.entidades.Cliente;
+import br.com.softwareservice.entidades.Funcionario;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -22,14 +26,34 @@ public class AtendimentoBean implements Serializable{
 	private Atendimento atendimento = new Atendimento();
 	private IDAO idao = new DAOFacade();
 	private Cliente cliente = new Cliente();
+
+	/*public Cliente buscarCliente(AjaxBehaviorEvent event){
+
+		InputText cpfCliente = (InputText) event.getSource();
+		String cpf = (String) cpfCliente.getValue();
+
+		cliente = iCliente.buscarCliente(cpf);
+		
+		if (cliente == null){
+			
+		}
+		
+		return cliente;
+
+	}*/
 	
-	/*public Cliente buscarCliente(AjaxBehaviorEvent event, String cpf){
-	 * 
-	 * InputText cpfCliente = (InputText) event.getSource();
-		String cpf = (String) cpfCliente.getValue()
-	 * 
-	 * cliente = iCliente.buscarCliente(cpf);
-	 * return cliente;
+	/*public Funcionario getFuncionario(AjaxBehaviorEvent event){
+		
+		InputText matriculaFuncionario = (InputText) event.getSource();
+		String matricula = (String) matriculaFuncionario.getValue();
+
+		cliente = iFuncionario.buscarFuncionario(matricula);
+		
+		if (cliente == null){
+			
+		}
+		
+		return cliente;
 		
 	}*/
 
