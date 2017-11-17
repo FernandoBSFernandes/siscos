abec82e80cf6af3b1bfec6c25f06c6c14bdb09e8
Fernando Barros <FernandoBSFernandes@users.noreply.github.com>
2017-11-17 16:23:40 -0200

    Bean de OS e Atendimento parcialmente finalizado.

--- src/main/java/br/com/softwareservice/controle/AtendimentoBean.java
+++ src/main/java/br/com/softwareservice/controle/AtendimentoBean.java
@@ -8,13 +8,13 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 
-import org.primefaces.component.inputtext.InputText;
-
+import org.primefaces.component.inputmask.InputMask;
 import br.com.softwareservice.dao.DAOFacade;
 import br.com.softwareservice.dao.IDAO;
 import br.com.softwareservice.entidades.Atendimento;
 import br.com.softwareservice.entidades.Cliente;
 import br.com.softwareservice.entidades.Funcionario;
+import br.com.softwareservice.entidades.OrdemServico;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -24,12 +24,14 @@ public class AtendimentoBean implements Serializable{
 	public AtendimentoBean() {}
 
 	private Atendimento atendimento = new Atendimento();
+	private OrdemServico ordemServico = new OrdemServico();
 	private IDAO idao = new DAOFacade();
 	private Cliente cliente = new Cliente();
+	private FacesMessage message;
 
-	/*public Cliente buscarCliente(AjaxBehaviorEvent event){
+	public Cliente buscarCliente(AjaxBehaviorEvent event){
 
-		InputText cpfCliente = (InputText) event.getSource();
+		InputMask cpfCliente = (InputMask) event.getSource();
 		String cpf = (String) cpfCliente.getValue();
 
 		cliente = iCliente.buscarCliente(cpf);
@@ -40,34 +42,41 @@ public class AtendimentoBean implements Serializable{
 		
 		return cliente;
 
-	}*/
+	}
 	
-	/*public Funcionario getFuncionario(AjaxBehaviorEvent event){
+	public Funcionario getFuncionario(AjaxBehaviorEvent event){
 		
-		InputText matriculaFuncionario = (InputText) event.getSource();
+		InputMask matriculaFuncionario = (InputMask) event.getSource();
 		String matricula = (String) matriculaFuncionario.getValue();
 
-		cliente = iFuncionario.buscarFuncionario(matricula);
+		Funcionario funcionario = iFuncionario.buscarFuncionario(matricula);
+		return funcionario;
 		
-		if (cliente == null){
-			
-		}
-		
-		return cliente;
-		
-	}*/
+	}
 
-	public void salvar(){
+	public void salvarAtendimentoEOrdemServico(){
+		try {
+			idao.salvar(atendimento);
+			idao.salvar(ordemServico);
+			message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atendimento e Ordem de Serviço registrados com sucesso.", null);
+
+		} catch (Exception e){
+			message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao tentar registrar o atendimento e/ou a ordem de serviço: " + e.getMessage(), null);
+
+		} finally {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, message);
+		}
+	}
+	
+	public void salvarAtendimento(){
 
 		FacesMessage msg = null;		
 		try {
 			idao.salvar(atendimento);
-			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atendimento registrado com sucesso", null);
-
-
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atendimento registrado com sucesso.", null);
 		} catch (Exception e){
 			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao tentar registrar o atendimento: " + e.getMessage(), null);
-
 		} finally {
 			FacesContext context = FacesContext.getCurrentInstance();
 			context.addMessage(null, msg);
@@ -90,4 +99,19 @@ public class AtendimentoBean implements Serializable{
 		this.cliente = cliente;
 	}
 
+	public OrdemServico getOrdemServico() {
+		return ordemServico;
+	}
+
+	public void setOrdemServico(OrdemServico ordemServico) {
+		this.ordemServico = ordemServico;
+	}
+
+	public IDAO getIdao() {
+		return idao;
+	}
+
+	public void setIdao(IDAO idao) {
+		this.idao = idao;
+	}
 }
